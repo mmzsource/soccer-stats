7a2fdcab3ba4a363f04ef1e602b386871803ac3c
MMz <mmz.source@gmail.com>
2016-08-06 17:35:44 +0200

    Refactor core.clj and add tests.

--- src/soccer_stats/core.clj
+++ src/soccer_stats/core.clj
@@ -1,32 +1,41 @@
+;; subl repl keyboard shortcuts:
+;; Press CTRL and , release both and then press either s, l, b, f for sending selection, line, block or file to repl
+
+;; Sublime workflow:
+;; Open project.clj and only then open sublime repl (cmd+shift+p repl)
+;; Open core.clj and send complete file to repl (ctrl+, f)
+
 (ns soccer-stats.core
   (:require 
      [clojure.java.io :as io]
      [clojure.edn :as edn]
      [clojure.pprint :as pp]))
 
-;; subl repl keyboard shortcuts:
-;; Press CTRL and , release both and then press either s, l, b, f for sending selection, line, block or file to repl
-
-(def positions
+(def file2edn
   (edn/read-string (slurp (io/resource "nederlandse-competitie-eindstanden.txt"))))
 
-(defn transform-positions [m k reps]
-  (zipmap (vals (k m)) (map #(repeat reps %) (keys (k m)))))
+(def weights (:weights file2edn))
 
-(def weighted-positions (merge-with concat 
-  (transform-positions positions :2006-2007 1) 
-  (transform-positions positions :2007-2008 2)
-  (transform-positions positions :2008-2009 3)
-  (transform-positions positions :2009-2010 4)
-  (transform-positions positions :2010-2011 5)
-  (transform-positions positions :2011-2012 6)
-  (transform-positions positions :2012-2013 7)
-  (transform-positions positions :2013-2014 8)
-  (transform-positions positions :2014-2015 9)
-  (transform-positions positions :2015-2016 10)))
+(def historic-ranks (:ranks file2edn))
+
+(defn invert [ranks]
+  (zipmap (keys ranks) (map clojure.set/map-invert (vals ranks))))
+
+(defn apply-weight [year-ranks year-weight]
+  (zipmap (keys year-ranks) (map #(repeat year-weight %) (vals year-ranks))))
+
+(defn calculate-weighted-ranks [ranks weights] 
+  (let [weight-keys (keys weights)]
+    (zipmap weight-keys (map #(apply-weight (% ranks) (% weights)) weight-keys))))
+
+(defn merge-ranks [ranks] (apply merge-with (cons concat (vals ranks))))
 
 (defn avg [coll] (double (/ (reduce + coll) (count coll))))
 
-(def sorted-positions (sort-by val < (zipmap (keys weighted-positions) (map #(avg %) (vals weighted-positions)))))
+(def weighted-ranks (calculate-weighted-ranks (invert historic-ranks) weights))
 
-(pp/pprint sorted-positions)
+(def merged-ranks (merge-ranks weighted-ranks))
+
+(def predicted-ranks (sort-by val < (zipmap (keys merged-ranks) (map #(avg %) (vals merged-ranks)))))
+
+(pp/pprint predicted-ranks)
--- test/soccer_stats/core_test.clj
+++ test/soccer_stats/core_test.clj
@@ -2,6 +2,51 @@
   (:require [clojure.test :refer :all]
             [soccer-stats.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-weights 
+  {:2015-2016 3 
+   :2014-2015 2})
+
+(def test-historic-ranks 
+  {:2015-2016 {1 "PSV" 2 "AJA" 3 "TWE"} 
+   :2014-2015 {1 "AJA" 2 "PSV" 3 "FEY"}})
+
+(def inverted-historic-ranks
+  {:2015-2016 {"PSV" 1 "AJA" 2 "TWE" 3}
+   :2014-2015 {"AJA" 1 "PSV" 2 "FEY" 3}})
+
+(deftest test-inversion
+  (testing "should result in map of same size"
+    (is (= (count (invert test-historic-ranks)) (count test-historic-ranks)))))
+
+(deftest test-rank-inversion
+  (testing "should make clubs the keys of the map and rank the values"
+    (is (= (:2015-2016 (invert test-historic-ranks)) {"PSV" 1 "AJA" 2 "TWE" 3}))
+    (is (= (:2014-2015 (invert test-historic-ranks)) {"AJA" 1 "PSV" 2 "FEY" 3}))
+    (is (= (:2015-2016 (invert test-historic-ranks)) (:2015-2016 inverted-historic-ranks)))
+    (is (= (:2014-2015 (invert test-historic-ranks)) (:2014-2015 inverted-historic-ranks)))))
+
+(deftest test-apply-weight-count
+  (testing "should result in map of same size"
+    (is (= 
+          (count (apply-weight (:2015-2016 inverted-historic-ranks) (:2015-2016 test-weights))) 
+          (count (:2015-2016 inverted-historic-ranks))))))
+
+(deftest test-apply-weight
+  (testing "should apply the weightfactor by repeating the rank <weight> number of times"
+    (is (= (apply-weight (:2015-2016 inverted-historic-ranks) 2) {"PSV" '(1 1) "AJA" '(2 2) "TWE" '(3 3)}))
+    (is (= (apply-weight (:2014-2015 inverted-historic-ranks) 1) {"AJA" '(1) "PSV" '(2) "FEY" '(3)}))))
+
+(deftest test-calculate-weighted-ranks
+  (testing "should apply weights to all rankmaps which are reffered to in the weights variable"
+    (is (= 
+          (calculate-weighted-ranks inverted-historic-ranks test-weights)
+          {:2015-2016 {"PSV" '(1 1 1) "AJA" '(2 2 2) "TWE" '(3 3 3)}
+           :2014-2015 {"AJA" '(1 1) "PSV" '(2 2) "FEY" '(3 3)}}))))
+
+(deftest test-merge-ranks
+  (testing "should merge all years into one map with club keys and weighted rank values"
+    (is (= 
+          (merge-ranks 
+            {:2015-2016 {"PSV" '(1 1 1) "AJA" '(2 2 2) "TWE" '(3 3 3)}
+             :2014-2015 {"AJA" '(1 1) "PSV" '(2 2) "FEY" '(3 3)}})
+          {"PSV" '(1 1 1 2 2) "AJA" '(2 2 2 1 1) "FEY" '(3 3) "TWE" '(3 3 3)}))))
